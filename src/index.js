1f4e32fa6ad203f01142f487948b31c912460cfa
tieulam <ttl.t954@gmail.com>
2020-06-28 22:27:24 -0600

    add how-to

--- src/index.js
+++ src/index.js
@@ -1,123 +1,15 @@
 import "./styles/index.scss";
+import "./styles/intro.scss";
+import "./styles/timer.scss";
 
 let _ = require("underscore");
-let Deck = require('./deck');
-import startTimer from './scripts/timer';
 import * as GameAPIUtil from './scripts/game';
 
-import { library, icon } from "@fortawesome/fontawesome-svg-core";
-import { faMoon, faStar, faSun } from "@fortawesome/free-solid-svg-icons";
-library.add(faMoon, faStar, faSun);
+document.getElementById("click-to-play").addEventListener("click", GameAPIUtil.startGame);
+document.getElementById("click-to-restart").addEventListener("click", function() { window.location.reload() });
 
-const d = new Deck();
-const cards = d.cards;
-const deck = d.deck;
-let matchesCount = 0;
-const matches = [0];
-let matchArr = [];
 
-const scoreSection = document.getElementById("score");
-const score = document.createElement("div");
-score.innerHTML = matchesCount;
-scoreSection.appendChild(score);
 
-document.getElementById("click-to-play").addEventListener("click", startTimer);
-
-function _renderDeck() {
-  document.getElementById("deck").innerHTML = "";
-
-  for (let i = 0; i < deck.length; i++) {
-    const card = deck[i];
-    let cardDiv = document.createElement("div");
-    let shapeSpan = document.createElement("span");
-    
-    switch (deck[i].shape) {
-      case deck[i].shape:
-        shapeSpan.innerHTML = icon({
-          prefix: "fas",
-          iconName: deck[i].shape,
-        }).html;
-        break;
-    }
-
-    shapeSpan.style.color = deck[i].color;
-    shapeSpan.className = "shape";
-    cardDiv.appendChild(shapeSpan);
-
-    cardDiv.style.background = deck[i].background;
-    cardDiv.className = "card";
-
-    // (card) => addCard(deck[i]);
-
-    cardDiv.addEventListener("click", function(card) {
-      if (matchArr.length < 3 && 
-        !matchArr.includes(card.id) &&
-        this.classList.contains("clicked")) {
-          matchArr.push(card.id);
-          console.log(matchArr);
-      } else {
-        matchArr = [];
-        matchArr.push(card.id);
-        console.log(matchArr);
-      }
-
-      if (matchArr.length === 3) {
-        matchArr.sort(function (a, b) {
-          return a - b;
-        });
-        // console.log(matchArr)
-        if (
-          GameAPIUtil.checkMatch(cards, matchArr) &&
-          !GameAPIUtil.checkMatchArr(matches, matchArr)
-        ) {
-          matchesCount++;
-          score.innerHTML = matchesCount;
-          matches.push(matchArr);
-          // console.log(matches)
-        }
-      }
-    });
-    cardDiv.addEventListener("click", function () {
-      if (this.classList.contains("clicked")) {
-        console.log(this)
-        this.classList.remove("clicked");
-      } else {
-        this.classList.add("clicked");
-      }
-    })
-    
-    document.getElementById("deck").appendChild(cardDiv);
-  }
-}
-
-_renderDeck();
-
-// add card.id clicked to matchArr
-// reset matchArr to an empty array & test for a match once matchArr.length === 3 
-function addCard(card) {
-  if (matchArr.length < 3 && !matchArr.includes(card.id)) {
-    matchArr.push(card.id);
-    console.log(matchArr);
-  } else {
-    matchArr = [];
-    matchArr.push(card.id);
-    console.log(matchArr);
-  }
-
-  if (matchArr.length === 3) {
-    matchArr.sort(function (a, b) {
-      return a - b;
-    });
-    // console.log(matchArr)
-    if (GameAPIUtil.checkMatch(cards, matchArr) && 
-      !GameAPIUtil.checkMatchArr(matches, matchArr)) {
-        matchesCount++;
-        score.innerHTML = matchesCount;
-        matches.push(matchArr);
-        // console.log(matches)
-    } 
-  }
-}
 
 
 
--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,7 +1,125 @@
 let _ = require("underscore");
+let Deck = require("../deck");
+import Swal from "sweetalert2";
+import startTimer from "./timer";
+
+import { library, icon } from "@fortawesome/fontawesome-svg-core";
+import { faMoon, faStar, faSun } from "@fortawesome/free-solid-svg-icons";
+library.add(faMoon, faStar, faSun);
+
+export function startGame() {
+  _renderDeck();
+  startTimer();
+  document
+    .getElementsByClassName("game-container")[0]
+    .classList.remove("hidden");
+  document.getElementsByClassName("intro")[0].classList.add("hidden");
+}
+
+const d = new Deck();
+const cards = d.cards;
+const deck = d.deck;
+let matchesCount = 0;
+const matches = [0];
+let matchArr = [];
+
+const scoreSection = document.getElementById("score");
+const score = document.createElement("div");
+score.innerHTML = matchesCount;
+scoreSection.appendChild(score);
+
+function _renderDeck() {
+  document.getElementById("deck").innerHTML = "";
+  const cardDivs = [];
+
+  for (let i = 0; i < deck.length; i++) {
+    let cardDiv = document.createElement("div");
+    let shapeSpan = document.createElement("span");
+
+    // add shapes to card 
+    switch (deck[i].shape) {
+      case deck[i].shape:
+        shapeSpan.innerHTML = icon({
+          prefix: "fas",
+          iconName: deck[i].shape,
+        }).html;
+        break;
+    }
+
+    shapeSpan.style.color = deck[i].color;
+    shapeSpan.className = "shape";
+    cardDiv.appendChild(shapeSpan);
+
+    cardDiv.style.background = deck[i].background;
+    cardDiv.className = "card";
+
+    cardDiv.addEventListener("click", function () {
+      let card = deck[i];
+
+      if (this.classList.contains("clicked")) {
+        this.classList.remove("clicked");
+        removeCard(card.id);
+        checkThree(cardDivs);
+      } else {
+        this.classList.add("clicked");
+        addCard(card.id);
+        checkThree(cardDivs);
+      }
+    });
+
+    cardDivs.push(cardDiv);
+    document.getElementById("deck").appendChild(cardDiv);
+  }
+}
+
+function addCard(cardId) {
+  matchArr.push(cardId);
+  console.log(matchArr)
+}
+
+function removeCard(cardId) {
+  matchArr = _.without(matchArr, cardId);
+  console.log(matchArr);
+}
+
+function checkThree(cardDivs) {
+  if (matchArr.length === 3) {
+    matchArr = matchArr.sort((a, b) => a - b);
+    if (
+      checkMatch(cards, matchArr) &&
+      !checkMatchArr(matches, matchArr)
+    ) {
+      matchesCount++;
+      score.innerHTML = matchesCount;
+      matches.push(matchArr);
+      console.log(matches);
+      matchArr = [];
+    } else if (!checkMatch(cards, matchArr)) {
+      Swal.fire({
+        position: "top-end",
+        text: "Not a match",
+        showConfirmButton: false,
+        timer: 800,
+      });
+      matchArr = [];
+    } else if (checkMatchArr(matches, matchArr)) {
+      Swal.fire({
+        position: "top-end",
+        text: "Already answered",
+        showConfirmButton: false,
+        timer: 800,
+      });
+      matchArr = [];
+    } else {
+      matchArr = [];
+    }
+
+    cardDivs.forEach((cardDiv) => cardDiv.classList.remove("clicked"));
+  }
+}
 
 // return boolean indicating if matchArr is a match or not 
-export function checkMatch(cards, matchArr) {
+function checkMatch(cards, matchArr) {
   let shapes = [], colors = [], backgrounds = [];
 
   matchArr.forEach(cardId => {
@@ -20,7 +138,7 @@ export function checkMatch(cards, matchArr) {
 }
 
 // return boolean if matchArr is included in matches 
-export function checkMatchArr(matches, matchArr) {
+function checkMatchArr(matches, matchArr) {
   for (let i = 0; i < matches.length; i++) {
     if (_.isEqual(matches[i], matchArr)) return true;
   }
