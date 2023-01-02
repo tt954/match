418d4514fc9a550e0fb9557efb77023c2dce8441
Tieulam Thai <ttl.t954@gmail.com>
2023-01-02 17:57:28 -0500

    set card index as button values

--- src/Game.ts
+++ src/Game.ts
@@ -13,12 +13,18 @@ class Game {
 
   start(): void {
     const d = new Deck(16);
+
     const deckContainer = document.querySelector('#deck');
     d.deck.forEach((card) => {
-      const cardElement = document.createElement('div');
+      const cardElement = document.createElement('button');
       cardElement.classList.add('card');
+      cardElement.value = card.value.toString();
       cardElement.style.backgroundColor = card.backgroundColor;
       deckContainer?.appendChild(cardElement);
+    });
+    deckContainer?.addEventListener('click', (event: Event) => {
+      const { target } = event;
+      if (target) console.log((target as HTMLButtonElement).value);
     });
   }
 }
